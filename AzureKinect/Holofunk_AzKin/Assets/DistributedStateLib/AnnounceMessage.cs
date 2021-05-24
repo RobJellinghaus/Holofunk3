39aec309a115ab456d95842f44147bb36ddbd94a
ROB-TUFLAPTOP <rjellinghaus@live.com>
2021-05-24 16:43:08 -0700

    Clean up the wrong places.

---
+++ AzureKinect/Holofunk_AzKin/Assets/DistributedStateLib/AnnounceMessage.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) 2020 by Rob Jellinghaus.
+
+namespace Distributed.State
+{
+    /// <summary>
+    /// Message sent by Peers that are just entering the system.
+    /// </summary>
+    /// <remarks>
+    /// Peers with no connections will periodically re-announce.
+    /// </remarks>
+    public class AnnounceMessage
+    {
+        /// <summary>
+        /// Socket address of the announcer.
+        /// </summary>
+        public SerializedSocketAddress AnnouncerSocketAddress { get; set; }
+
+        /// <summary>
+        /// The announcer intends to host audio for the whole group.
+        /// </summary>
+        /// <remarks>
+        /// This is the case if the announcer, for example, is a PC hosting all the sound hardware for the space.
+        /// </remarks>
+        public bool AnnouncerIsHostingAudio { get; set; }
+
+        /// <summary>
+        /// The socket addresses of peers already known to this Peer.
+        /// </summary>
+        public SerializedSocketAddress[] KnownPeers { get; set; }
+    }
+}
