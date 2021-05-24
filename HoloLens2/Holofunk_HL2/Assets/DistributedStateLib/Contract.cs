e0f10b0160e648593239582ccd95c53ed560bd6e
ROB-TUFLAPTOP <rjellinghaus@live.com>
2021-05-24 12:34:27 -0700

    Put both DistributedStateLib and LiteNetLib code right in the top-level project folder.

---
+++ HoloLens2/Holofunk_HL2/Assets/DistributedStateLib/Contract.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) 2020 by Rob Jellinghaus.
+using System.Diagnostics;
+
+namespace Distributed.State
+{
+    public static class Contract
+    {
+        public static void Requires(bool condition, string message = null)
+        {
+            if (!condition)
+            {
+                Debug.Assert(condition, message);
+            }
+        }
+    }
+}
--- copy_litenetlib_and_distributedstate_down.cmd
+++ copy_litenetlib_and_distributedstate_down.cmd
@@ -1,9 +1,12 @@
+pushd c:\git\Holofunk3
 rmdir /s /q HoloLens2\Holofunk_HL2\Assets\LiteNetLib
-rmdir /s /q HoloLens2\Holofunk_HL2\\Assets\DistributedState
+rmdir /s /q HoloLens2\Holofunk_HL2\Assets\DistributedStateLib
 robocopy /s ..\LiteNetLib\LiteNetLib HoloLens2\Holofunk_HL2\Assets\LiteNetLib *.cs
-robocopy /s ..\DistributedStateTest HoloLens2\Holofunk_HL2\Assets\DistributedState *.cs
-rmdir /s /q HoloLens2\Holofunk_HL2\Assets\LiteNetLib\obj
-rmdir /s /q HoloLens2\Holofunk_HL2\Assets\DistributedState\DistributedStateLib\obj
-rmdir /s /q HoloLens2\Holofunk_HL2\Assets\DistributedState\DistributedStateTest
-rmdir /s /q HoloLens2\Holofunk_HL2\Assets\DistributedState\DistributedThing\obj
+robocopy /s ..\DistributedState\DistributedStateLib HoloLens2\Holofunk_HL2\Assets\DistributedStateLib *.cs
+
+rem Not sure these are needed at all...
+rem rmdir /s /q HoloLens2\Holofunk_HL2\Assets\LiteNetLib\obj
+rem rmdir /s /q HoloLens2\Holofunk_HL2\Assets\DistributedState\DistributedStateLib\obj
+rem rmdir /s /q HoloLens2\Holofunk_HL2\Assets\DistributedState\DistributedStateTest
+rem rmdir /s /q HoloLens2\Holofunk_HL2\Assets\DistributedState\DistributedThing\obj
 
