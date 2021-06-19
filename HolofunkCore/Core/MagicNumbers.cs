1ba3ad90e0e3ba8bc53b7d8ff108ec56b89a1b36
Rob Jellinghaus <rjellinghaus@live.com>
2021-06-19 16:36:51 -0700

    Propagate core

---
+++ HolofunkCore/Core/MagicNumbers.cs
@@ -0,0 +1,15 @@
+﻿/// Copyright (c) 2021 by Rob Jellinghaus. All rights reserved.
+
+namespace Holofunk.Core
+{
+    /// <summary>
+    /// Constants that are determined by hand-tuning or otherwise outside of Holofunk.
+    /// </summary>
+    public static class MagicNumbers
+    {
+        /// <summary>
+        /// Number of frames over which to average, when smoothing positions.
+        /// </summary>
+        public const int FramesToAverageWhenSmoothing = 10;
+    }
+}
