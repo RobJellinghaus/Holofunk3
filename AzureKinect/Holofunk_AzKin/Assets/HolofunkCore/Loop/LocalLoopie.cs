adb7b38274476c919c2128853d5ea78d8f703d14
Rob Jellinghaus <rjellinghaus@live.com>
2021-09-05 15:50:16 -0700

    flat cube is not cube

--- AzureKinect/Holofunk_AzKin/Assets/HolofunkCore/Loop/LocalLoopie.cs
+++ AzureKinect/Holofunk_AzKin/Assets/HolofunkCore/Loop/LocalLoopie.cs
@@ -135,7 +135,7 @@ namespace Holofunk.Loop
             for (int i = 0; i < MagicNumbers.OutputBinCount; i++)
             {
                 // really a very flat sort of cube... not much of a cube at all really
-                GameObject disc = ShapeContainer.InstantiateShape(ShapeType.Cube, firstBeatMeasureControllerObject);
+                GameObject disc = ShapeContainer.InstantiateShape(ShapeType.FlatCube, firstBeatMeasureControllerObject);
                 disc.SetActive(true);
                 disc.transform.localPosition = new Vector3(0, i * MagicNumbers.FrequencyDiscVerticalDistance, 0);
                 Vector3 localScale = disc.transform.localScale;
