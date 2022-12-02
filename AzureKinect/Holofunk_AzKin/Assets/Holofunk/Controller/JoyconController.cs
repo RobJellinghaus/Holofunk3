815bac76a543ebd567d1136f8db71836b9b452ed
Rob Jellinghaus <rjellinghaus@live.com>
2022-12-02 13:49:32 -0800

    Snapshot.

---
+++ AzureKinect/Holofunk_AzKin/Assets/Holofunk/Controller/JoyconController.cs
@@ -0,0 +1,383 @@
+﻿/// Copyright by Rob Jellinghaus.  All rights reserved.
+
+using Distributed.State;
+using Holofunk.Core;
+using Holofunk.Distributed;
+using Holofunk.Hand;
+using Holofunk.Loop;
+using Holofunk.Perform;
+using Holofunk.Sound;
+using Holofunk.StateMachines;
+using Holofunk.Viewpoint;
+using Holofunk.VolumeWidget;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Holofunk.Controller
+{
+    using HandStateMachineInstance = StateMachineInstance<JoyconEvent>;
+
+    /// <summary>
+    /// Tracks the state and interactions of a player's controller.
+    /// </summary>
+    /// <remarks>
+    /// Arguably this is a horrible amount of code duplication vs. the HandController in the HoloLens 2 code
+    /// base, but pending some actual revival of the mixed reality version, not worrying about that right now.
+    /// </remarks>
+    public class JoyconController : MonoBehaviour, IModel
+    {
+        [Tooltip("Which hand.")]
+        public Side handSide = Side.Left;
+
+        /// <summary>
+        /// The state machine for handling the player's state.
+        /// </summary>
+        private HandStateMachineInstance stateMachineInstance;
+
+        /// <summary>
+        /// The loopie currently being held by this controller.
+        /// </summary>
+        /// <remarks>
+        /// This is only ever non-null when the stateMachineInstance is in recording state.
+        /// </remarks>
+        private GameObject currentlyHeldLoopie;
+
+        /// <summary>
+        /// What should this controller do when it touches a loopie?
+        /// </summary>
+        /// <remarks>
+        /// This will be applied repeatedly on every update in which a loopie is touched, so idempotency is strongly recommended!
+        /// </remarks>
+        private Action<DistributedLoopie> touchedLoopieAction;
+
+        /// <summary>
+        /// What should this controller do on every update, before touching loopies?
+        /// </summary>
+        /// <remarks>
+        /// This is used for, e.g., updating the current volume widget and performing other interactions that aren't
+        /// loopie-centric.
+        /// TODO: could touchedLoopieAction just be a sort of updateAction?
+        /// </remarks>
+        private Action updateAction;
+
+        /// <summary>
+        /// The sorted IDs of the loopies touched by this controller.
+        /// </summary>
+        /// <remarks>
+        /// Recalculated anew on every frame.
+        /// </remarks>
+        private readonly List<DistributedId> touchedLoopieIds = new List<DistributedId>();
+
+        /// <summary>
+        /// The previous contents of touchedLoopies (for debugging).
+        /// </summary>
+        private readonly List<DistributedId> previouslyTouchedLoopieIds = new List<DistributedId>();
+
+        /// <summary>
+        /// The (sorted) IDs of the loopies currently touched by this controller.
+        /// </summary>
+        public IEnumerable<DistributedId> TouchedLoopieIds => touchedLoopieIds;
+
+        /// <summary>
+        /// Should the set of touched loopies be kept stable, rather than recomputed on each update?
+        /// </summary>
+        /// <remarks>
+        /// When using a popup menu created by touching some loopie(s), we do not want to un-touch the loopies while picking from
+        /// the menu. Setting this flag causes the set of touched loopies to be stabilized for the duration of the flag.
+        /// There is probably a better way to think about this, but pending a fuller reactive or other UI structure,
+        /// we'll live with this.
+        /// </remarks>
+        internal bool KeepTouchedLoopiesStable { get; set; }
+
+        /// <summary>
+        /// Any loopies touched by this controller?
+        /// </summary>
+        internal bool AnyLoopiesTouched => touchedLoopieIds.Count > 0;
+
+        internal void SetUpdateAction(Action updateAction)
+            => this.updateAction = updateAction;
+
+        internal void SetTouchedLoopieAction(Action<DistributedLoopie> touchedLoopieAction)
+            => this.touchedLoopieAction = touchedLoopieAction;
+
+        private void ApplyToTouchedLoopies(Action<DistributedLoopie> action)
+        {
+            if (action != null)
+            {
+                foreach (LocalLoopie localLoopie in
+                    DistributedObjectFactory.FindComponentInstances<LocalLoopie>(
+                        DistributedObjectFactory.DistributedType.Loopie, includeActivePrototype: false))
+                {
+                    if (touchedLoopieIds.Contains(localLoopie.DistributedObject.Id))
+                    {
+                        action((DistributedLoopie)localLoopie.DistributedObject);
+                    }
+                }
+            }
+        }
+
+        /*
+        /// <summary>
+        /// Get the PerformerController component from our parent..
+        /// </summary>
+        internal PerformerPreController PerformerController => gameObject.transform.parent.gameObject.GetComponent<PerformerPreController>();
+        */
+
+        /// <summary>
+        /// Get the DistributedPerformer component from our parent.
+        /// </summary>
+        internal DistributedPerformer DistributedPerformer => gameObject.transform.parent.gameObject.GetComponent<DistributedPerformer>();
+
+        /// <summary>
+        /// for debugging only
+        /// </summary>
+        internal string HandStateMachineInstanceString => stateMachineInstance?.ToString() ?? "";
+
+        // Update is called once per frame
+        void Update()
+        {
+            PerformerState performer = DistributedPerformer.GetPerformer();
+
+            if (PerformerController.OurPlayer.PerformerHostAddress == default(SerializedSocketAddress))
+            {
+                // if we have a state machine, shut it down now.
+                if (stateMachineInstance != null)
+                {
+                    stateMachineInstance.OnCompleted();
+                    stateMachineInstance = null;
+                }
+
+                // we aren't recognized yet... state machine not running.
+                return;
+            }
+
+            // if we don't have a state machine instance yet, then we should now create one!
+            // (it exists only as long as we are recognized by the viewpoint.)
+            if (stateMachineInstance == null)
+            {
+                stateMachineInstance = new HandStateMachineInstance(HandPoseEvent.Opened, HandStateMachine.Instance, this);
+            }
+
+            // Update the hand state, based on the latest known Kinect hand state.
+            HandPoseValue currentHandPose = HandPose(ref performer);
+
+            // Pass the performer state down by ref for efficiency (no mutation please, it'll be lost)
+            UpdateHandState(currentHandPose, ref performer);
+
+            // Update the loopie's position while the user is holding it.
+            if (currentlyHeldLoopie != null && DistributedViewpoint.Instance != null)
+            {
+                Vector3 performerHandPosition = HandPosition(ref performer);
+                Matrix4x4 localToViewpointMatrix = DistributedViewpoint.Instance.LocalToViewpointMatrix();
+                if (localToViewpointMatrix != Matrix4x4.zero)
+                {
+                    Vector3 viewpointHandPosition = localToViewpointMatrix.MultiplyPoint(performerHandPosition);
+                    currentlyHeldLoopie.GetComponent<DistributedLoopie>().SetViewpointPosition(viewpointHandPosition);
+                }
+            }
+
+            // Update touched loopies first, before updating hand position and state.
+            if (KeepTouchedLoopiesStable)
+            {
+                // do nothing! the previous TouchedLoopieIdList can remain the same.
+                // Even if some or all of the loopies in it have been deleted, the effect
+                // of this is benign because when we go to actually traverse them, we
+                // will skip them.
+            }
+            else
+            {
+                // freely update the touched loopie list of the performer
+                UpdateTouchedLoopieList(ref performer);
+            }
+
+            // apply the update action, if any
+            if (updateAction != null)
+            {
+                updateAction();
+            }
+
+            ApplyToTouchedLoopies(touchedLoopieAction);
+        }
+
+        /// <summary>
+        /// Update the local lists of loopies touched by this hand.
+        /// </summary>
+        private void UpdateTouchedLoopieList(ref PerformerState performer)
+        {
+            touchedLoopieIds.Clear();
+
+            Vector3 handPosition = HandPosition(ref performer);
+
+            foreach (LocalLoopie localLoopie in
+                DistributedObjectFactory.FindComponentInstances<LocalLoopie>(
+                    DistributedObjectFactory.DistributedType.Loopie, includeActivePrototype: false))
+            {
+                Vector3 loopiePosition = localLoopie.transform.position;
+                if (Vector3.Distance(loopiePosition, handPosition) < MagicNumbers.HandRadius)
+                {
+                    touchedLoopieIds.Add(localLoopie.DistributedObject.Id);
+                }
+            }
+
+            touchedLoopieIds.Sort(DistributedId.Comparer.Instance);
+
+            // logging changes in touched loopie list
+            if (!EqualLists(previouslyTouchedLoopieIds, touchedLoopieIds))
+            {
+                previouslyTouchedLoopieIds.Clear();
+                previouslyTouchedLoopieIds.AddRange(touchedLoopieIds);
+                HoloDebug.Log($"Updating {handSide} touched loopies: prior [{string.Join(", ", previouslyTouchedLoopieIds)}], current [{string.Join(", ", touchedLoopieIds)}]");
+            }
+        }
+
+        private bool EqualLists(List<DistributedId> list1, List<DistributedId> list2)
+        {
+            if (list1.Count != list2.Count)
+            {
+                return false;
+            }
+            for (int i = 0; i < list1.Count; i++)
+            {
+                if (list1[i] != list2[i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public HandPoseValue HandPose(ref PerformerState performer) => handSide == Side.Left
+            ? performer.LeftHandPose
+            : performer.RightHandPose;
+
+        public Vector3 HandPosition(ref PerformerState performer) => handSide == Side.Left
+            ? performer.LeftHandPosition
+            : performer.RightHandPosition;
+
+        /// <summary>
+        /// Update the hand state, and if appropriate, create an event and pass it to the state machine.
+        /// Note that BodyRelativeHandPosition may override currentHandState.
+        /// </summary>
+        /// <param name="handPose">Current (smoothed) hand pose from the performer</param>
+        /// <param name="performer">The actual performer, passed by ref for efficiency (no mutation please)</param>
+        private void UpdateHandState(HandPoseValue handPose, ref PerformerState performer)
+        {
+            HandPoseEvent handPoseEvent = HandPoseEvent.FromHandPose(handPose);
+
+            if (!lastHandPose.HasValue)
+            {
+                // This is the very first hand state ever.
+                lastHandPose = handPose;
+
+                // Create state machine instance.
+                stateMachineInstance = new HandStateMachineInstance(HandPoseEvent.Unknown, HandStateMachine.Instance, this);
+
+                // TODO: do we do an immediate OnNext? If not, then shouldn't we be setting lastHandPose to Unknown here?
+                if (handPose != HandPoseValue.Unknown)
+                {
+                    stateMachineInstance.OnNext(handPoseEvent);
+                }
+            }
+            else
+            {
+                if (lastHandPose.Value != handPose)
+                {
+                    lastHandPose = handPose;
+                    
+                    stateMachineInstance.OnNext(handPoseEvent);
+
+                    HoloDebug.Log(HandStateMachineInstanceString);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Create a new loopie at the current hand's position, and set it as the currentlyHeldLoopie.
+        /// </summary>
+        public void CreateLoopie()
+        {
+            Holofunk.Core.Contract.Requires(currentlyHeldLoopie == null);
+
+            if (DistributedViewpoint.Instance == null)
+            {
+                HoloDebug.Log("No DistributedViewpoint.TheViewpoint; can't create loopie");
+                return;
+            }
+
+            Vector3 viewpointHandPosition = GetViewpointHandPosition();
+
+            GameObject newLoopie = DistributedLoopie.Create(viewpointHandPosition);
+            currentlyHeldLoopie = newLoopie;
+        }
+
+        /// <summary>
+        /// Create a volume widget.
+        /// </summary>
+        public GameObject CreateVolumeWidget()
+        {
+            if (DistributedViewpoint.Instance == null)
+            {
+                HoloDebug.Log("No DistributedViewpoint.TheViewpoint; can't create volume widget");
+                return null;
+            }
+
+            Vector3 viewpointHandPosition = GetViewpointHandPosition();
+
+            GameObject newWidget = DistributedVolumeWidget.Create(viewpointHandPosition);
+            return newWidget;
+        }
+
+        public Vector3 GetViewpointHandPosition()
+        {
+            // performer space hand position
+            Vector3 localHandPosition = GetLocalHandPosition();
+            Matrix4x4 localToViewpointMatrix = DistributedViewpoint.Instance.LocalToViewpointMatrix();
+            Vector3 viewpointHandPosition = localToViewpointMatrix.MultiplyPoint(localHandPosition);
+            return viewpointHandPosition;
+        }
+
+        public Vector3 GetLocalHandPosition()
+        {
+            PerformerState performer = DistributedPerformer.GetPerformer();
+
+            // performer space hand position
+            Vector3 performerHandPosition = HandPosition(ref performer);
+            return performerHandPosition;
+        }
+
+        /// <summary>
+        /// Let go of the currentlyHeldLoopie, leaving it at its world space position.
+        /// </summary>
+        public void ReleaseLoopie()
+        {
+            if (currentlyHeldLoopie == null)
+            {
+                //Debug.Log("HandController.ReleaseLoopie(): currentlyHeldLoopie is null and should not be!");
+            }
+            else
+            {
+                currentlyHeldLoopie.GetComponent<DistributedLoopie>().FinishRecording();
+
+                currentlyHeldLoopie = null;
+            }
+        }
+
+        /// <summary>
+        /// Apply the given sound effect to the set of touched loopies, by appending it to their effect lists.
+        /// </summary>
+        public void ApplySoundEffectToTouchedLoopies(EffectId effect)
+        {
+            foreach (LocalLoopie localLoopie in
+                DistributedObjectFactory.FindComponentInstances<LocalLoopie>(
+                    DistributedObjectFactory.DistributedType.Loopie, includeActivePrototype: false))
+            {
+                if (touchedLoopieIds.Contains(localLoopie.DistributedObject.Id))
+                {
+                    localLoopie.AppendSoundEffect(effect);
+                }
+            }
+        }
+    }
+    */
+}
---
+++ AzureKinect/Holofunk_AzKin/Assets/Holofunk/Controller/JoyconEvent.cs
@@ -0,0 +1,105 @@
+﻿/// Copyright by Rob Jellinghaus.  All rights reserved.
+
+using Holofunk.Core;
+using Holofunk.Hand;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Holofunk.Controller
+{
+    /// <summary>A Joycon event in a state machine.</summary>
+    public struct JoyconEvent
+    {
+        /// <summary>
+        /// Which button?
+        /// </summary>
+        readonly Joycon.Button _button;
+        /// <summary>
+        /// Is the button down?
+        /// </summary>
+        readonly bool _isDown;
+        /// <summary>
+        /// Has this event been captured by another UI layer?
+        /// </summary>
+        readonly bool _isCaptured;
+
+        /// <summary>The button that was down or up.</summary>
+        internal Joycon.Button Button => _button;
+
+        /// <summary>
+        /// Is the button down? (if not, it's up)
+        /// </summary>
+        internal bool IsDown => _isDown;
+
+        /// <summary>
+        /// Has this event already been captured?
+        /// </summary>
+        /// <remarks>
+        /// We may still want to respond to already-captured events in some way (e.g. by changing a hand sprite),
+        /// but we want to be able to detect that the capture already took place, and modify the transition
+        /// appropriately.
+        /// </remarks>
+        internal bool IsCaptured => _isCaptured;
+
+        internal JoyconEvent(Joycon.Button button, bool isDown) { _button = button; _isDown = isDown; _isCaptured = false; }
+
+        /// <summary>
+        /// Construct a captured JoyconEvent from an existing one.
+        /// </summary>
+        internal JoyconEvent AsCaptured() { return new JoyconEvent(this); }
+
+        private JoyconEvent(JoyconEvent other) { _button = other._button; _isDown = other._isDown; _isCaptured = true; }
+
+        public static bool operator ==(JoyconEvent l, JoyconEvent r)
+        {
+            return l.Button == r.Button && l.IsDown == r.IsDown && l.IsCaptured == r.IsCaptured;
+        }
+
+        public static bool operator !=(JoyconEvent l, JoyconEvent r)
+        {
+            return !(l == r);
+        }
+
+        public override string ToString()
+        {
+            return Button.ToString() + (_isDown ? "(down)" : "(up)") + (_isCaptured ? "(captured)" : "");
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is JoyconEvent @event && (this == @event);
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = 1793886979;
+            hashCode = hashCode * -1521134295 + _button.GetHashCode();
+            hashCode = hashCode * -1521134295 + _isDown.GetHashCode();
+            hashCode = hashCode * -1521134295 + _isCaptured.GetHashCode();
+            return hashCode;
+        }
+    }
+
+    public class JoyconEventComparer : IComparer<JoyconEvent>
+    {
+        internal static readonly JoyconEventComparer Instance = new JoyconEventComparer();
+
+        public int Compare(JoyconEvent x, JoyconEvent y)
+        {
+            int delta = (int)x.Button - (int)y.Button;
+            if (delta != 0) return delta;
+            if (x.IsDown != y.IsDown)
+            {
+                return x.IsDown ? -1 : 1;
+            }
+            if (x.IsCaptured != y.IsCaptured)
+            {
+                return x.IsCaptured ? 1 : -1;
+            }
+            return 0;
+        }
+    }
+}
